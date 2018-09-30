cde0b6acc2df17b8eed14041c409b624c94f7c92
ozkan <ozkanbe@gmail.com>
2018-09-30 17:47:30 +0200

    project move

---
+++ DecoratorDesignPatternExample.Core/Domain/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DecoratorDesignPatternExample.Core.Domain
+{
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    public class Book : LibraryItem
+    {
+        private string _author;
+        private string _title;
+
+        // Constructor
+        public Book(string author, string title, int numCopies)
+        {
+            _author = author;
+            _title = title;
+            NumCopies = numCopies;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nBook ------ ");
+            Console.WriteLine(" Author: {0}", _author);
+            Console.WriteLine(" Title: {0}", _title);
+            Console.WriteLine(" # Copies: {0}", NumCopies);
+        }
+    }
+}
---
+++ DecoratorDesignPatternExample.Core/Domain/LibraryItem.cs
@@ -0,0 +1,19 @@
+﻿namespace DecoratorDesignPatternExample.Core.Domain
+{
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    public abstract class LibraryItem
+    {
+        private int _numCopies;
+
+        // Property
+        public int NumCopies
+        {
+            get { return _numCopies; }
+            set { _numCopies = value; }
+        }
+
+        public abstract void Display();
+    }
+}
